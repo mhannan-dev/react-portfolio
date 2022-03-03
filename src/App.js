47ae79392f7bb66505383140888131815c56a4c6
MHannan <mdhannan.info@gmail.com>
2022-03-04 00:20:29 +0600

    Done general css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Exprience from './components/experience/Exprience';
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Portfolio from './components/portfolio/Portfolio';
+import Service from './components/service/Service';
+import Testimonial from './components/testimonial/Testimonial';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header />
+     <Nav />
+     <About />
+     <Exprience />
+     <Service />
+     <Portfolio />
+     <Testimonial />
+     <Contact />
+    </>
   );
 }
 
